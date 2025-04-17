2caf49a34b8be11f09d4733db98c86aa0c4164aa
chriistinahu <55038678+chriistinahu@users.noreply.github.com>
2025-04-17 04:48:07 +0000

    fix postgressql connection string

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ if(builder.Environment.IsDevelopment())
 else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
+        // use POSTGRESQL connection instead!!
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")));
     builder.Services.AddStackExchangeRedisCache(options =>
     {
